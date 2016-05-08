3e79accf0379abdba9a73a8c5a67ae2b1778f50c
aid666 <aid666@gmail.com>
2016-05-08 18:58:53 +0800

    correct service json

--- assembleService.sh
+++ assembleService.sh
@@ -5,9 +5,9 @@ SVC_NAME=$(grep "name=" service.properties | awk '{print substr($1, 6)}')
 ARCHIVE_NAME=$(ls build/libs/*.jar | awk '{print substr($1, 12)}')
 
 cp build/libs/$ARCHIVE_NAME /opt/jars/.
-cp template/consul.json /etc/consul.d/.
+cp template/consul.json /etc/consul.d/${SVC_NAME}.json
 mkdir -p /etc/service/${SVC_NAME}
 cp template/startService.sh /etc/service/${SVC_NAME}/run
 
-sed -i 's,'"<serviceName>"','"$SVC_NAME"',g' /etc/consul.d/consul.json
+sed -i 's,'"<serviceName>"','"$SVC_NAME"',g' /etc/consul.d/${SVC_NAME}.json
 sed -i 's,'"<archiveName>"','"$ARCHIVE_NAME"',g' /etc/service/${SVC_NAME}/run
